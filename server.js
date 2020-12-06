3e9ad98e7ccf52fac70f9c2b5cc211ce7ee949ee
Eduardo Lopez <elopez@avascent.com>
2020-12-06 09:21:03 -0500

    added and tested the app.get and app.post functions.

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
 
+const entries = [{id: 1, name: 'course1'},
+{id: 2, name: 'course2'},
+{id: 3, name: 'course3'},
+{id: 4, name: 'course4'}];
+
 
 app.use(express.static('website'));
+app.use(express.json());
+app.use(express.urlencoded( {extended: true}));
+
+app.get('/entries', (req, res) => {
+    res.send(entries)
+});
+
+app.post('/entries', (req, res) => {
+
+    const entry = {
+        id: entries.length + 1,
+        name: req.body.name
+    };
+
+    entries.push(entry);
+    res.send(entry);
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
