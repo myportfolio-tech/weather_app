2090dfe5c4985c785cb631c9cded58e386d1b715
Eduardo Lopez <elopez@avascent.com>
2020-12-07 20:17:43 -0500

    client-side js format - changed the functions to called the weather API

--- website/JS/app.js
+++ website/JS/app.js
@@ -9,67 +9,87 @@ const lat = document.getElementById('lat');
 const long = document.getElementById('long'); 
 const temp = document.getElementById('temp'); 
 const mood = document.getElementById('mood'); 
+const full = document.getElementById('full'); 
 const iconimage = document.getElementById('icon-image'); 
 
 
-btn.addEventListener("click", function() {
-    callAPI(zip.value, mood_pick.value);
-});
+btn.addEventListener("click", clickEvent);
 
-// async function startProcess() {
-//     try{
-//         const weather = await callAPI(zip.value, user.value);
-//         console.log('Return: ', weather)
-//         displayBox.innerHTML = weather;//.main.temp;
-//         }
-//     catch (err){
-//         console.log(err.message);
-//     }
-// }
+// async function() {
+//     let response = await callAPI(zip.value, mood_pick.value);
+//     console.log(response);
+
+async function clickEvent() {
+    try{
+        const response = await callAPI(zip.value);
+        // console.log('Return: ', response)
+        const rBody = formRequestBody(response);
+        console.log(rBody);
+        }
+    catch (err){
+        console.log(err.message);
+    }
+}
+
+function formRequestBody(response){
+    
+    const data = {
+        temp: response.main.temp,
+        lat: response.coord.lat,
+        lon: response.coord.lon,
+        user: mood_pick.value
+    };
+
+    console.log(data);
+
+    return data
+}
+
+async function callAPI(zip){
+        const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=81c30a80ddd5b3a82bcf35083a43ed9c`
+        let response = await fetch(url);
+        let resJSON = await response.json()
+
+        // console.log(data);
+        // console.log(data.main.temp);
+        return resJSON;
+        
+}
 
 
-// async function callAPI(zip){
-//         const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=81c30a80ddd5b3a82bcf35083a43ed9c`
-//         let response = await fetch(url);
-//         let data = await response.json()
 
+// async function callAPI(zip, user){
+
+//     fetch('/weather', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             zipcode: zip
+//         })
+
+//     }).then(res => res.json()).then(data => {
 //         console.log(data);
-//         console.log(data.main.temp);
-//         return data
+//         weatherData(data, user);
+//     });
+
 // }
 
-async function callAPI(zip, user){
-
-    fetch('/weather', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            zipcode: zip
-        })
-
-    }).then(res => res.json()).then(data => {
-        console.log(data);
-        weatherData(data, user);
-    });
-
-}
 
 
+// function weatherData(data, user) {
 
-function weatherData(data, user) {
+//     date.textContent = `DATE: ${data.date}`;
+//     city.textContent = `City: ${data.city}`;
+//     wind.textContent = `Wind: ${data.wind}`;
+//     lat.textContent = `Lattitude: ${data.lat}`;
+//     long.textContent = `Longitude: ${data.long}`;
+//     temp.textContent = `TEMP: ${data.temp}`;
+//     mood.textContent = `Mood: ${user}`;
 
-    date.textContent = `DATE: ${data.date}`;
-    city.textContent = `City: ${data.city}`;
-    wind.textContent = `Wind: ${data.wind}`;
-    lat.textContent = `Lattitude: ${data.lat}`;
-    long.textContent = `Longitude: ${data.long}`;
-    temp.textContent = `TEMP: ${data.temp}`;
-    mood.textContent = `Mood: ${user}`;
+//     var iconurl = "http://openweathermap.org/img/w/" + data.icon + ".png";
+//     iconimage.innerHTML = `<img src="${iconurl}" alt="">`
 
-    var iconurl = "http://openweathermap.org/img/w/" + data.icon + ".png";
-    iconimage.innerHTML = `<img src="${iconurl}" alt="">`
-
-}
+// }
